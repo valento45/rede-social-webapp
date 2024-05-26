bcf78ebe8b5e45806d3a05e01d26bb0a6962ee79
Igor Carvalho <iggor1935@hotmail.com>
2024-05-26 20:17:51 -0300

    Ajustes

---
+++ src/Rede.Social/Rede.Social.Domain/Bases/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rede.Social.Domain.Bases
+{
+    public class Pessoa
+    {
+
+        public int IdPessoa { get; set; }
+        public string Nome { get; set; }
+        public string RG { get;}
+        public DateTime DataNascimento { get; set; }
+        public string EstadoCivil { get; set; }
+        public string CEP { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string Pais { get; set; }
+
+        public Pessoa()
+        {
+            
+        }
+
+
+
+    }
+}
---
+++ src/Rede.Social/bd_social.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE bd_social
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+	
+	
+	create table pessoa_tb(
+		IdPessoa serial not null primary key,
+		Nome varchar(300) not null,
+		RG varchar(50),
+		DataNascimento timestamp not null,
+		EstadoCivil varchar(100),
+		CEP varchar(30),
+		Logradouro varchar(300),
+		Numero integer,
+		Cidade varchar(100),
+		UF varchar(5),
+		Pais varchar(50)		
+	);
+	
+	create table usuario_tb(
+		IdUsuario serial not null primary key,
+		IdPessoa integer not null,
+		Email varchar(200) not null,
+		Senha varchar not null,
+		AutenticaDoisFatores boolean,
+		Celular varchar(50),
+		Verificado boolean,
+		CONSTRAINT id_pessoa_fk foreign key(IdPessoa)
+		references pessoa_tb(IdPessoa)
+	
+	);
