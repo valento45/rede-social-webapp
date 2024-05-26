5351e18582a7ef9f15b7107a1d19aefc0485f599
Igor Carvalho <iggor1935@hotmail.com>
2024-05-26 20:24:10 -0300

    Add project Security

--- src/Rede.Social/Rede.Social.Domain/Usuarios/Usuario.cs
+++ src/Rede.Social/Rede.Social.Domain/Usuarios/Usuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rede.Social.Domain.Bases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -10,6 +11,33 @@ namespace Rede.Social.Domain.Usuarios
     public class Usuario
     {
 
-        
+        public int IdUsuario { get; set; }
+        public int IdPessoa { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public bool AutenticaDoisFatores { get; set; }
+        public string Celular { get; set; }
+        public bool EmailVerificado { get; set; }
+        private Pessoa _pessoa { get; set; }
+        public Pessoa Pessoa
+        {
+            get
+            {
+                if(_pessoa == null)
+                    _pessoa = new Pessoa(); 
+
+                return _pessoa;
+            }
+            set
+            {
+                _pessoa = value;
+            }
+        }
+
+
+        public Usuario()
+        {
+
+        }
     }
 }
