a78050fee634c05aaa191d6cbed24e6ab6f3231c
ADILSON CARVALHO <adilson.carvalho01@fatec.sp.gov.br>
2024-05-26 23:18:47 -0300

    Feed Screen created

---
+++ src/Rede.Social/Rede.Social.WebApp/Controllers/FeedController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rede.Social.Service.Interfaces;
+
+namespace Rede.Social.WebApp.Controllers
+{
+    public class FeedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Feed");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Feed()
+        {
+            return View();
+        }
+
+    }
+}
