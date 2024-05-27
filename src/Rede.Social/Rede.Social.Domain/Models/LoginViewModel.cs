2d344b2597eea15dfe44cfd7998a19a7d6620b17
Igor Carvalho <iggor1935@hotmail.com>
2024-05-26 22:31:27 -0300

    Ajustes

---
+++ src/Rede.Social/Rede.Social.Domain/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rede.Social.Domain.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Message { get; set; }
+
+        public LoginViewModel()
+        {
+            
+        }
+    }
+}
--- src/Rede.Social/Rede.Social.WebApp/Controllers/HomeController.cs
+++ src/Rede.Social/Rede.Social.WebApp/Controllers/HomeController.cs
@@ -1,21 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rede.Social.Domain.Models;
+using Rede.Social.Repository.Interfaces;
+using Rede.Social.Service.Interfaces;
 using Rede.Social.WebApp.Models;
 using System.Diagnostics;
 
 namespace Rede.Social.WebApp.Controllers
 {
+    [Route("[controller]")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUsuarioService _usuarioService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUsuarioService usuarioService)
         {
             _logger = logger;
+            _usuarioService = usuarioService;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("Logar")]
+        public async Task<IActionResult> Logar(LoginViewModel loginViewModel)
+        {
+
+            
+            throw new Exception();
         }
 
         public IActionResult Privacy()
